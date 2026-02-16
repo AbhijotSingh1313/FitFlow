d7c69dc0d8b00cbfe99f6b98404a0a75897a227b
Abhijot Singh <abhisng2007@gmail.com>
2026-02-16 21:59:08 +0530

    created basic user table in database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.core.config import settings
 from app.database.session import engine, Base
+from app.models import user
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
