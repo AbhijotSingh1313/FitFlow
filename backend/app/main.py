119fbdb8457773f95061e82208ef027d7cdf9ef4
Abhijot Singh <abhisng2007@gmail.com>
2026-02-16 21:23:31 +0530

    initial database integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.database.session import engine, Base
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
@@ -7,3 +8,4 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 @app.get("/")
 def root():
     return {"message": f"{settings.PROJECT_NAME} backend is alive"}
+Base.metadata.create_all(bind=engine)
