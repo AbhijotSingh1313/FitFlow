e1128673a844c497983f5632601c04150da35f61
Abhijot Singh <abhisng2007@gmail.com>
2026-02-17 21:57:00 +0530

    Created User Repository

---
+++ backend/app/repositories/user_repo.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models import User
+
+
+def create_user(db: Session, email: str, password_hash: str):
+    user = User(
+        email=email,
+        password_hash=password_hash
+    )
+
+    db.add(user)      # to put into session
+    db.commit()       # to save permanently
+    db.refresh(user)  # to get saved data 
+
+    return user
