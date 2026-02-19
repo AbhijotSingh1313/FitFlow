92faf9d8613a85ca43006a67d684dabff68b9689
Abhijot Singh <abhisng2007@gmail.com>
2026-02-19 13:06:48 +0530

    implement JWT authentication with OAuth2 password flow

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -8,6 +8,9 @@ from app.schemas.user_schema import UserLogin
 from app.services.user_service import login_user
 from fastapi import HTTPException
 from app.core.auth import create_access_token
+from app.core.auth import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
 
 router = APIRouter()
 
@@ -21,16 +24,28 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
 
     return {"id": created_user.id, "email": created_user.email}
 
+
 @router.post("/login")
-def login(user: UserLogin, db: Session = Depends(get_db)):
-    existing_user = login_user(db, user.email, user.password)
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    existing_user = login_user(
+        db,
+        form_data.username,   # email goes here
+        form_data.password
+    )
 
     if not existing_user:
         raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    token = create_access_token({"sub": existing_user.email})
+    access_token = create_access_token({"sub": existing_user.email})
 
     return {
-        "access_token": token,
+        "access_token": access_token,
         "token_type": "bearer"
     }
+
+@router.get("/me")
+def read_current_user(current_user: str = Depends(get_current_user)):
+    return {"logged_in_as": current_user}
--- backend/app/core/auth.py
+++ backend/app/core/auth.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timedelta
 from jose import jwt
+from jose import JWTError, jwt
+from fastapi import HTTPException, status
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 
 SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
@@ -13,3 +17,27 @@ def create_access_token(data: dict):
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token"
+            )
+
+        return email
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token is invalid or expired"
+        )
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    email = verify_access_token(token)
+    return email
