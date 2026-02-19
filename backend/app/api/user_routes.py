cf95030e780b6b65c811a15c6afd6b63b6310690
Abhijot Singh <abhisng2007@gmail.com>
2026-02-19 12:37:03 +0530

    added JWT Authentication

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -7,7 +7,7 @@ from app.database.dependency import get_db
 from app.schemas.user_schema import UserLogin
 from app.services.user_service import login_user
 from fastapi import HTTPException
-
+from app.core.auth import create_access_token
 
 router = APIRouter()
 
@@ -28,4 +28,9 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     if not existing_user:
         raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    return {"message": "Login successful", "email": existing_user.email}
+    token = create_access_token({"sub": existing_user.email})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
