690ad814c9871bc644ecb4ed6dee7207f879fafe
Abhijot Singh <abhisng2007@gmail.com>
2026-02-19 12:07:23 +0530

    strong password validation added
    password should be more than 8 characters long containing letters digits symbols

--- backend/app/schemas/user_schema.py
+++ backend/app/schemas/user_schema.py
@@ -1,8 +1,26 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, field_validator
+import re
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
+
+    @field_validator("password")
+    def validate_password(cls, value):
+
+        if len(value) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+
+        if not re.search(r"[A-Za-z]", value):
+            raise ValueError("Password must contain a letter")
+
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Password must contain a number")
+
+        if not re.search(r"[!@#$%^&*()_\-+=<>?/{}~|]", value):
+            raise ValueError("Password must contain a special symbol")
+
+        return value
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
