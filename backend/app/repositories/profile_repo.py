8b7f0d51f29a7fc00ad0064d4032248509ded11b
Abhijot Singh <abhisng2007@gmail.com>
2026-02-21 12:20:27 +0530

    minor Exception handeling

--- backend/app/repositories/profile_repo.py
+++ backend/app/repositories/profile_repo.py
@@ -1,10 +1,14 @@
 from sqlalchemy.orm import Session
 from app.models.profile import Profile
 from app.models.user import User
-
+from fastapi import HTTPException
 
 def create_profile(db: Session, user_email: str, data):
     user = db.query(User).filter(User.email == user_email).first()
+
+    existing_profile = db.query(Profile).filter(Profile.user_id == user.id).first()
+    if existing_profile:
+        raise HTTPException(status_code=400, detail="Profile already exists")
 
     profile = Profile(
         name=data.name,
@@ -19,7 +23,6 @@ def create_profile(db: Session, user_email: str, data):
     db.commit()
     db.refresh(profile)
     return profile
-
 
 def get_my_profile(db: Session, user_email: str):
     user = db.query(User).filter(User.email == user_email).first()
