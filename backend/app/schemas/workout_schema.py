200ac3259ee52aed48336f31c973d082565fa299
Abhijot Singh <abhisng2007@gmail.com>
2026-03-02 22:59:24 +0530

    Added exercises to workouts

--- backend/app/schemas/workout_schema.py
+++ backend/app/schemas/workout_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 
 class WorkoutCreate(BaseModel):
@@ -6,9 +7,23 @@ class WorkoutCreate(BaseModel):
     level_id: int
 
 
+class WorkoutExerciseResponse(BaseModel):
+    id: int
+    exercise_id: Optional[int]
+    sets: Optional[int]
+    reps: Optional[int]
+    weight: Optional[int]
+    is_custom: bool
+    custom_name: Optional[str]
+
+    class Config:
+        from_attributes = True
+
+
 class WorkoutResponse(BaseModel):
     id: int
     date: str
+    workout_exercises: List[WorkoutExerciseResponse]
 
     class Config:
         from_attributes = True
